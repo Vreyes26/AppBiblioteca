4e790023b838315f9d626619758ec0e661719dd2
Vreyes26 <Vreyes26@users.noreply.github.com>
2020-10-11 14:09:03 -0300

    Initial commit

---
+++ app/src/main/java/com/example/biblioteca/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.biblioteca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usertb, passtb;
+    private Button loginBtn;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usertb =(EditText)findViewById(R.id.user_tb);
+        passtb =(EditText)findViewById(R.id.pass_tb);
+        loginBtn =(Button)findViewById(R.id.buttonLogin);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Task().execute();
+            }
+        });
+    }
+
+    class Task extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            for(int i = 1; i <= 5; i++)
+            {
+                try
+                {
+                    Thread.sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            progressBar.setVisibility(View.INVISIBLE);
+            Intent i = new Intent(getBaseContext(), home_act.class);
+            startActivity(i);
+        }
+    }
+}
